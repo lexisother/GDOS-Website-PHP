134192df480fe04e7a99c68d4dc572fc2e0991c4
Alyxia Sother <lexisoth2005@gmail.com>
2021-11-23 21:48:38 +0100

    [database] Add new values to seeder and migrations

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -16,6 +16,8 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'age' => $this->faker->numberBetween(14, 20),
+            'role' => $this->faker->randomElement(['Software Developer', 'Device Manager']),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
