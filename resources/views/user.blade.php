097a1a22c084dd0aec1e0b5584aeca5e70a6d64b
Alyxia Sother <lexisoth2005@gmail.com>
2021-11-23 21:09:43 +0100

    [views/user] Add the necessary error handling

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
     <div class="container">
-        <h1>{{ $user->name }}</h1>
+        @if($user)
+            <h1>{{ $user->name }}</h1>
+        @else
+            <h1>User not found</h1>
+        @endif
     </div>
 @endsection
