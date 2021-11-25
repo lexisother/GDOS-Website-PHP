42e7cd388d7b9fd6f14f2b45b0a32b39901b37c4
Alyxia Sother <lexisoth2005@gmail.com>
2021-11-25 12:38:52 +0100

    [views/errors] Add 413

---
+++ resources/views/errors/413.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Too Large'))
+@section('code', '413')
+@section('message', __('Request entity too large.'))
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -8,10 +8,6 @@
             <a style="text-decoration: none" href="/user/{{ $user->name }}">
                 <div class="authcard">
                     <div class="authcardHeader">
-                        {{-- TODO Add a URL to the member image, or perhaps
-                        even store the raw image data inside of the DB (no idea
-                        how I'll end up displaying this data but as you know I
-                        always find a way --}}
                         <img src="/storage/uploads/avatars/{{ $user->avatar }}" height="110px">
                         <p class="name">{{  $user->name }}</p>
                     </div>
