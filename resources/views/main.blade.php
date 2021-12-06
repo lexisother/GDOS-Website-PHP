d5dbc0dcaf085d1567cb4e3148a265196decf8b0
Alyxia Sother <lexisoth2005@gmail.com>
2021-12-06 10:55:38 +0100

    [views/main] Add warning for profile incompletion

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,6 +2,13 @@
 
 @section('content')
 <div class="container">
+    @auth
+        @if (Auth::user()->role == "No role.")
+            <div class="alert alert-danger">
+                Your profile is incomplete! Please go to your <a href="/profile">profile settings</a> to resolve.
+            </div>
+        @endif
+    @endauth
     <div class="authcards">
         {{-- TODO Component-ify this!!! IMPORTANT!!! --}}
         @foreach ($users as $user)
